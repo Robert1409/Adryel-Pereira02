19b2e89ae65ecc55a6a69532043096ab216d3e8a
Robert1409 <Robert09060713@gmail.com>
2021-03-23 10:25:40 -0300

    first commit

---
+++ src/zeusmenu.js
@@ -0,0 +1,40 @@
+const adryelpereiramenu = (prefix) => {
+	return `
+
+            COMANDOS:
+
+
+  *Comandos do Adryel-Pereira.*
+
+➸ *${prefix}loli*
+➸ *${prefix}hentai*
+➸ *${prefix}dono*
+➸ *${prefix}porno*
+➸ *${prefix}boanoite*
+➸ *${prefix}bomdia*
+➸ *${prefix}boatarde*
+➸ *${prefix}mia*
+➸ *${prefix}mia1*
+➸ *${prefix}mia2*
+➸ *${prefix}belle*
+➸ *${prefix}belle1*
+➸ *${prefix}belle2*
+➸ *${prefix}belle3*
+➸ *${prefix}ayeko*
+
+╔════════════════════
+  FEITO POR *ADRYEL-PEREIRA*
+  DUVIDAS? 👇
+  WA.me/EM BREVE...
+╚════════════════════`
+}
+
+exports.adryel-pereiramenu = adryelpreiramenu
+
+
+
+
+
+
+
+
